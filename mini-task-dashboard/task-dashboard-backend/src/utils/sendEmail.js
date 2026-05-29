4affcc2f54e4dd8c1209879aa5311884c9907d85
unknown <paridhijain153@gmail.com>
2026-05-29 15:10:05 +0530

    checking existing variable rendering

--- mini-task-dashboard/task-dashboard-backend/src/utils/sendEmail.js
+++ mini-task-dashboard/task-dashboard-backend/src/utils/sendEmail.js
@@ -9,6 +9,8 @@ const transporter = nodemailer.createTransport({
 });
 
 async function sendOTPEmail(email, otp) {
+  console.log("EMAIL_USER exists:", !!process.env.EMAIL_USER);
+  console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
   console.log("Trying to send email to:", email);
 
   const info = await transporter.sendMail({
