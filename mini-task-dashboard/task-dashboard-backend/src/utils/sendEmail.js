c14fbdcb1643c49158893e977e881ba1c201ab0f
unknown <paridhijain153@gmail.com>
2026-05-29 15:15:36 +0530

    Updating sendEmail.js

--- mini-task-dashboard/task-dashboard-backend/src/utils/sendEmail.js
+++ mini-task-dashboard/task-dashboard-backend/src/utils/sendEmail.js
@@ -6,6 +6,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 async function sendOTPEmail(email, otp) {
@@ -13,18 +16,26 @@ async function sendOTPEmail(email, otp) {
   console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
   console.log("Trying to send email to:", email);
 
-  const info = await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "TaskFlow OTP Verification",
-    html: `
-      <h2>Your OTP is:</h2>
-      <h1>${otp}</h1>
-      <p>This OTP will expire in 10 minutes.</p>
-    `,
-  });
+  try {
+    await transporter.verify();
+    console.log("SMTP connection verified");
 
-  console.log("Email sent:", info.messageId);
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "TaskFlow OTP Verification",
+      html: `
+        <h2>Your OTP is:</h2>
+        <h1>${otp}</h1>
+        <p>This OTP will expire in 10 minutes.</p>
+      `,
+    });
+
+    console.log("Email sent:", info.messageId);
+  } catch (error) {
+    console.error("MAIL ERROR:", error);
+    throw error;
+  }
 }
 
 module.exports = sendOTPEmail;
