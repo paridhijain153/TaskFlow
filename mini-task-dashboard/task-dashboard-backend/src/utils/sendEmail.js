d9c562199fc821847aca90b5b7c8c78a75718809
unknown <paridhijain153@gmail.com>
2026-05-29 23:27:39 +0530

    Switch to brevo api

--- mini-task-dashboard/task-dashboard-backend/src/utils/sendEmail.js
+++ mini-task-dashboard/task-dashboard-backend/src/utils/sendEmail.js
@@ -1,28 +1,34 @@
-const nodemailer = require("nodemailer");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.BREVO_LOGIN,
-    pass: process.env.BREVO_SMTP_KEY,
-  },
-});
+const client = SibApiV3Sdk.ApiClient.instance;
+
+client.authentications["api-key"].apiKey =
+  process.env.BREVO_API_KEY;
+
+const apiInstance =
+  new SibApiV3Sdk.TransactionalEmailsApi();
 
 async function sendOTPEmail(email, otp) {
-  const info = await transporter.sendMail({
-    from: '"TaskFlow" <paridhijain153@gmail.com>',
-    to: email,
-    subject: "TaskFlow OTP Verification",
-    html: `
-      <h2>Your OTP is:</h2>
-      <h1>${otp}</h1>
-      <p>This OTP will expire in 10 minutes.</p>
-    `,
-  });
+  const result =
+    await apiInstance.sendTransacEmail({
+      sender: {
+        email: "paridhijain153@gmail.com",
+        name: "TaskFlow",
+      },
+      to: [
+        {
+          email,
+        },
+      ],
+      subject: "TaskFlow OTP Verification",
+      htmlContent: `
+        <h2>Your OTP is:</h2>
+        <h1>${otp}</h1>
+        <p>This OTP will expire in 10 minutes.</p>
+      `,
+    });
 
-  console.log("EMAIL SENT:", info.messageId);
+  console.log("BREVO RESULT:", result);
 }
 
 module.exports = sendOTPEmail;
